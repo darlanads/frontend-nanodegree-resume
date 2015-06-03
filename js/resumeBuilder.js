d570ef09e840d858f08925428a2a863f950e31f7
Darlan Araújo dos Santos <darlan.ads@gmail.com>
2015-06-03 00:12:32 -0300

    Treinamento javascript

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,4 +1,13 @@
-$("#main").append("Darlan Araújo dos Santos");
+// $("#main").append("Darlan Araújo dos Santos");
 
 var awesomeThoughts = "I am Darlan and I am AWESOME";
+var funThoughts = awesomeThoughts.replace("AWESOME", "FUN");
 console.log(awesomeThoughts);
+console.log(funThoughts);
+
+// $("#main").append(funThoughts);
+
+var formattedName = HTMLheaderName.replace("%data%", "Darlan");
+var formattedRole = HTMLheaderRole.replace("%data%", "Role");
+
+$("#header").append(formattedName).prepend(formattedRole);
